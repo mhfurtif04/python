a005b0524a43318290e5100bc6298ea6d7af262c
mhfurtif04 <mn0543702@gmail.com>
2024-12-17 12:47:37 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+x=int(input("devinez le chiffre auquel je pense situer entre 1 et 100"))
+if x<20:
+    print("plus haut")
+if x==46:
+    print("felicitation,c'est le nombre 46")
+if x>46:
+    print("plus bas")
+if x<46:
+    print("plus haut")
+if x>70:
+    print("plus bas,vous etes loin du compte")
+if x<20:
+    print("plus haut,vous etes loin du compte")
+
+
+
